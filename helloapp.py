6c42a07fc27558a79a5f970fcda4989a2f3c0c79
jerinthomas703 <116787849+jerinthomas703@users.noreply.github.com>
2026-04-27 12:19:54 +0530

    Update helloapp.py
    oomkilled testcase

--- helloapp.py
+++ helloapp.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+data = []
+
 @app.route('/')
 def hello():
-    return "Hello, DevOps World!"
+    while True:
+        data.append("A" * 10**6)
+    return "Hello"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
